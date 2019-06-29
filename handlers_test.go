982478fb99b9842b792bc7d82a475143b91c3c70
oliverpauffley <mrpauffley@gmail.com>
2019-06-29 17:08:38 +0000

    Added auth test with a authed user

--- handlers_test.go
+++ handlers_test.go
@@ -132,7 +132,7 @@ func TestLoginHandler(t *testing.T) {
 func TestAuthMiddleware(t *testing.T) {
 
 	t.Run("Unauthorized users are rejected", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodPost, "/register", nil)
+		req, _ := http.NewRequest(http.MethodPost, "testing", nil)
 		response := httptest.NewRecorder()
 
 		handler := AuthMiddleware(getVoidHandler())
@@ -144,25 +144,37 @@ func TestAuthMiddleware(t *testing.T) {
 			t.Errorf("Expected %v got %v", want, got)
 		}
 	})
-	//
-	// 	t.Run("Authorized users are accepted", func(t *testing.T){
-	// 		req, _ := http.NewRequest(http.MethodPost, "/register", nil)
-	// 		response := httptest.NewRecorder()
-	//
-	// 		handler := AuthMiddleware(getTestHandler())
-	// 		handler.ServeHTTP(response, req)
-	//
-	// 		want := http.StatusOK
-	// 		got := response.Code
-	// 		if want != got {
-	// 			t.Errorf("Expected %v got %v", want, got)
-	// 		}
-	// 		wantString := "success!"
-	// 		gotString  := response.Body.String()
-	// 		if gotString != wantString {
-	// 			t.Errorf("Inner handler does not run, wanted %s got %s", wantString, gotString)
-	// 		}
-	// 	})
+
+	t.Run("Authorized users are accepted", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, "/testing", nil)
+		response := httptest.NewRecorder()
+
+		// set user as authenticated
+		session, err := store.Get(req, "authentication-cookie")
+		if err != nil {
+			t.Fatal("should be no error here")
+		}
+		user := User{ID: 1, Authenticated: true}
+		session.Values["user"] = user
+		err = session.Save(req, response)
+		if err != nil {
+			t.Fatal("should be no error here")
+		}
+
+		handler := AuthMiddleware(getTestHandler())
+		handler.ServeHTTP(response, req)
+
+		want := http.StatusOK
+		got := response.Code
+		if want != got {
+			t.Errorf("Expected %v got %v", want, got)
+		}
+		wantString := "success!"
+		gotString := response.Body.String()
+		if gotString != wantString {
+			t.Errorf("Inner handler does not run, wanted %s got %s", wantString, gotString)
+		}
+	})
 }
 
 // getVoid Handler returns a http.HandlerFunc for testing http middleware, should never run!
